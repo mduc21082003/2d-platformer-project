a08aa8d7f0deec3906eacb901453488d74cc8638
Duc Nguyen <mduc21082003@gmail.com>
2024-10-17 00:24:19 +0700

    design tilemap, physic player (basic)

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -35,7 +35,7 @@ public class MovementController : MonoBehaviour
         float targetSpeed = speed * speedMultiplier * relativeTransform.x;
 
         rb.velocity = new Vector2(targetSpeed, rb.velocity.y);
-        isWallTouch = Physics2D.OverlapBox(wallCheckPoint.position, new Vector2(0.06f, 0.8f), 0, wallLayer);
+        isWallTouch = Physics2D.OverlapBox(wallCheckPoint.position, new Vector2(0.06f, 0.6f), 0, wallLayer);
 
         if (isWallTouch)
         {
